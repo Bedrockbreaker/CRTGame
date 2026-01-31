6fd2d66028c9d972a6c78c128302ab11040cc955
Bedrockbreaker <coolmandude101@gmail.com>
2026-01-31 12:43:05 -0700

    Add dragging sheets around

---
+++ CRTGame/Scripts/Sheet.cs
@@ -0,0 +1,75 @@
+using Godot;
+using Godot.Collections;
+
+namespace CRTGame;
+
+public partial class Sheet : Area2D
+{
+	[Export]
+	public int ColorLayer;
+
+	[Export]
+	public Color Color = Colors.White;
+
+	private Vector2 DragOffset;
+	private bool bDragging;
+
+	public override void _Ready()
+	{
+		CollisionLayer = 1U << (ColorLayer + 8);
+		CollisionMask = 1U << (ColorLayer + 8);
+
+		Modulate = Color;
+
+		ZIndex = ColorLayer + 8;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Click", true))
+		{
+			// Test mouse overlap with sheet (or other sheets on top of this one)
+
+			PhysicsPointQueryParameters2D raycast = new()
+			{
+				Position = GetGlobalMousePosition(),
+				CollisionMask = ~0U ^ 255U, // Ignore layers 0-7
+				CollideWithAreas = true,
+				CollideWithBodies = false
+			};
+
+			Array<Dictionary> result = GetWorld2D().DirectSpaceState.IntersectPoint(raycast);
+
+			foreach (Dictionary hit in result)
+			{
+				// Ignore overlaps with non-sheet objects
+				if (hit["collider"].As<GodotObject>() is not Sheet other) continue;
+
+				// Ignore sheets on top of this one
+				if (other.ColorLayer > ColorLayer)
+				{
+					bDragging = false;
+					return;
+				}
+
+				// Pick the arbitrary first sheet if they share the same color layer
+				if (other.ColorLayer == ColorLayer && other != this)
+				{
+					bDragging = false;
+					return;
+				}
+
+				// Ignore sheets below, but continue to check the remaining sheets
+				if (other.ColorLayer < ColorLayer) continue;
+
+				bDragging = true;
+				DragOffset = raycast.Position - Position;
+			}
+		}
+
+		bDragging &= Input.IsActionPressed("Click");
+		if (!bDragging) return;
+
+		Position = GetGlobalMousePosition() - DragOffset;
+	}
+}
