1116e233c1b19e4bc5cf7d18afae55c772939e09
Bedrockbreaker <coolmandude101@gmail.com>
2026-02-01 04:26:41 -0700

    Put win screen on level 9

--- CRTGame/Scripts/Win_Screen.cs
+++ CRTGame/Scripts/Win_Screen.cs
@@ -1,9 +1,10 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class Win_Screen : Sprite2D
+public partial class Win_Screen : ColorRect
 {
 	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 		Hide();
